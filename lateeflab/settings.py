ab61dbf82d6608e260fba60f065d176d2bfe6af2
heyylateef <lateef.lotz@gmail.com>
2022-04-08 16:23:51 -0400

    Use S3 storage backend, debug=false

--- lateeflab/settings.py
+++ lateeflab/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key() 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG= os.getenv("DEBUG", "True") 
+DEBUG= False 
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
